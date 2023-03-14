779b11f1c435096bea7de80a61a325260e1f080d
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-15 00:57:54 +0600

    bugs : handlePaymentInfo  fixed

--- handlePayment.js
+++ handlePayment.js
@@ -2,5 +2,8 @@ const handlePaymentInfo=async (id)=>{
     const data = await fetch(`ROOMS.json`);
     const result = await data.json();
     const roomInfo=result.find(r => r._id==id)
-    document.getElementById('paymentModalLevel').innerText = roomInfo.name
+    console.log(roomInfo)
+    document.getElementById('paymentModalLevel').innerText = roomInfo.name;
+    document.getElementById('exampleFormControlTextarea1').innerText = roomInfo.description
+    ;
 }
